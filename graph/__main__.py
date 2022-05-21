e618f156e35b8d0596e47ba993d01684d457a869
AwdeDarkar <darkardengeno@gmail.com>
2022-05-21 14:35:05 -0400

    Add Apache 2.0 license

---
+++ graph/__main__.py
@@ -0,0 +1,9 @@
+"""
+__main__.py
+CLI entry-point for the graph generator and related tools.
+
+Author: Ben Croisdale
+
+Copyright (c) 2022 Ben Croisdale. All rights reserved.
+Released under the Apache 2.0 license as described in the file LICENSE.
+"""
